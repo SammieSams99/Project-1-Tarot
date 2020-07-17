835871247d28c0c157bbaee7c9bc873cba396ddc
sammiesams <sammielh2013@gmail.com>
2020-07-17 15:12:54 -0700

    Additional files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 console.log("Yay Project 1!")
 
 // Upon Load:
-    // The top center of the screen will have 
+    // The top center of the screen will have the name of the game and a tagline of some
+    // sort that fits the charecter of the game.
+
+    // Center of the page stays empty but will be filled in later
+
+    // Bottom of the page will have cards layed out. It should look like there are multiple cards  The player will only see the 
+    // back of the cards. 
+
+    // When the player is hovering over the cards they will float to give a more interactive feel.
+
+
+    // There will be a clickable button under the cards that says "Pick Your Daily Card". This will be 
+    
+    // When the player is hovering over the cards they will float to give a more interactive feel.
+
+    // When the 
+
